3d47e516f4c3a1d97dffd2cdc74e29bf53b54cd5
Son Roy Almerol <sonroya.almerol@gmail.com>
2022-05-20 12:05:15 -0400

    remove unnecessary code

--- ArrowFunctions/index.js
+++ ArrowFunctions/index.js
@@ -1,28 +1,3 @@
-// var hello;
-
-// hello = () => {
-//   document.getElementById("demo").innerHTML += this;
-// }
-
-// //The window object calls the function:
-// window.addEventListener("load", hello);
-
-// //A button object calls the function:
-// document.getElementById("btn").addEventListener("click", hello);
-
-
-// var numbers = [1,2,3,4,5];
-// const squares = numbers.map(n => n * n);
-// document.write(squares);
-
-// var evens = new Array();
-// numbers.forEach(n => {
-//    if (n % 2 === 0) { 
-//       evens.push(n);
-//    }
-// });
-// document.write(evens);
-
 const author = {
     fullName: "Bob Alice",
     books: ["Book 01","Book 02","Book 03"],
